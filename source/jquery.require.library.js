d344a4e375feaebaf2f0c817599e1eb80d14a11b
Jason Ramos <jason@stackideas.com>
2012-02-20 15:04:32 +0800

    Added foundry library require loader.

---
+++ source/jquery.require.library.js
@@ -0,0 +1,35 @@
+/**
+ * jquery.require.library
+ * Foundry script loader.
+ *
+ * Copyright (c) 2011 Jason Ramos
+ * www.stackideas.com
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+$.require.addLoader('library', function() {
+
+	var batch = this,
+		args = $.makeArray(arguments),
+		options,
+		names;
+
+	// Expand arguments into its actual definition
+	if ($.isPlainObject(args[0])) {
+		options = args[0];
+		names = args.slice(1);
+	} else {
+		names = args;
+	}
+
+	$.extend(options, {
+		path: Foundry.scriptPath
+	});
+
+	return batch.script.apply(batch, [options].concat(names));
+
+});
